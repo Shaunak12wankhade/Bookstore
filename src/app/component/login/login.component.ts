3359e48e0811eb64e6b9523cab6c6812e280a63d
unknown <shaunak12wankhade@ggmail.com>
2022-03-15 16:14:08 +0530

    [Shaunak] Added: Refactored code for login component

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -12,7 +12,7 @@ export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
   submitted = false;
 
-  constructor(private formbuilder: FormBuilder, private user: UserService, private route:Router) { }
+  constructor(private formbuilder: FormBuilder, private user: UserService, private route: Router) { }
 
   ngOnInit(): void {
 
@@ -20,7 +20,7 @@ export class LoginComponent implements OnInit {
       email: ['', Validators.required],
       password: ['', Validators.required],
       selectoption: ['', Validators.required],
-     
+
     })
   }
 
@@ -34,37 +34,39 @@ export class LoginComponent implements OnInit {
 
         email: this.loginForm.value.email,   // leftside email is exactly same as that of backend API and rightside email i.e., email should be exact same as that of formcontrolname in .html file or same as written above in ngonit 
         password: this.loginForm.value.password
-       
+
       }
 
-      if(this.loginForm.value.selectoption == '1') {  // this selectoption is coming from login.html from formControlName and is used to select whether user wants to login or admin wants to login
-      this.user.userlogin(payload).subscribe((response: any) => {    //subscribe is a method from observable
-        console.log(response)
-        
-        localStorage.setItem('token', response.result.accessToken);  // this accessToken is coming from swagger and we can see inside console also
+      if (this.loginForm.value.selectoption == '1') {  // this selectoption is coming from login.html from formControlName and is used to select whether user wants to login or admin wants to login
+        this.user.userlogin(payload).subscribe((response: any) => {    //subscribe is a method from observable
+          console.log(response)
 
-          localStorage.setItem('fullName',response.result.fullName);
+          localStorage.setItem('token', response.result.accessToken);  // this accessToken is coming from swagger and we can see inside console also
+
+          localStorage.setItem('fullName', response.result.fullName);
           localStorage.setItem('mobileNo', response.result.phone);
 
-        //  localStorage.setItem('email', response.email);
+          //  localStorage.setItem('email', response.email);
 
-        this.route.navigateByUrl("/dashboard")  //by doing this after clicking on login button we will get redirected to dashboard
+          this.route.navigateByUrl("/dashboard")  //by doing this after clicking on login button we will get redirected to dashboard
 
-        // this.route.navigateByUrl("/dashboard/admin")
-      })
-    }else if(this.loginForm.value.selectoption == '2') { // this selectoption is coming from login.html from formControlName and is used to select whether user wants to login or admin wants to login 
-      this.user. adminlogin(payload).subscribe((response: any) => {    //subscribe is a method from observable
-        console.log(response)
-        localStorage.setItem('token', response.result.accessToken);  // this accessToken is coming from swagger
+          // this.route.navigateByUrl("/dashboard/admin")
+        })
+      } else if (this.loginForm.value.selectoption == '2') { // this selectoption is coming from login.html from formControlName and is used to select whether user wants to login or admin wants to login 
+        this.user.adminlogin(payload).subscribe((response: any) => {    //subscribe is a method from observable
+          console.log(response)
+          localStorage.setItem('token', response.result.accessToken);  // this accessToken is coming from swagger
 
-        this.route.navigateByUrl("/dashboard/admin") //by doing this after clicking on login button we will get redirected to admin dashboard
-      })
-    }
+          // localStorage.setItem('email', response.result.email);
+          // localStorage.setItem('mobileNo', response.result.phone);
+          this.route.navigateByUrl("/dashboard/admin") //by doing this after clicking on login button we will get redirected to admin dashboard
+        })
+      }
     } else {
       console.log("enter data");
     }
 
-    
+
   }
 
   // userlogin(){
