bcb7200a8cb7901e193018a40ca20fc26bb5a50e
unknown <shaunak12wankhade@ggmail.com>
2022-03-15 16:10:02 +0530

    [Shaunak] Added: Refactored code for http service

--- src/app/services/httpservice/http.service.ts
+++ src/app/services/httpservice/http.service.ts
@@ -6,27 +6,27 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class HttpService {
-  baseurl= environment.baseurl
-  constructor( private http :HttpClient) { }
+  baseurl = environment.baseurl
+  constructor(private http: HttpClient) { }
 
-  postService(url:any, data:any, token:boolean=false, httpOptions:any){   // here data:any is used in context of whatever data we are sending to backend through post operation
+  postService(url: any, data: any, token: boolean = false, httpOptions: any) {   // here data:any is used in context of whatever data we are sending to backend through post operation
     console.log(data)
-    return this.http.post(this.baseurl+url, data, token && httpOptions)
-   }
+    return this.http.post(this.baseurl + url, data, token && httpOptions)
+  }
 
-   
-  getService(url:any, token:boolean=false, httpOptions:any){   // no need to take data:any as we are not sending any data to backend rather we are getting data from backend
+
+  getService(url: any, token: boolean = false, httpOptions: any) {   // no need to take data:any as we are not sending any data to backend rather we are getting data from backend
     // console.log(data)
-    return this.http.get(this.baseurl+url,token && httpOptions)
-   }
+    return this.http.get(this.baseurl + url, token && httpOptions)
+  }
 
-   putService(url:any, data:any, token:boolean=false, httpOptions:any){   // here data:any is used in context of whatever data we are sending to backend through post operation
+  putService(url: any, data: any, token: boolean = false, httpOptions: any) {   // here data:any is used in context of whatever data we are sending to backend through post operation
     console.log(data)
-    return this.http.put(this.baseurl+url, data, token && httpOptions)
-   }
+    return this.http.put(this.baseurl + url, data, token && httpOptions)
+  }
 
-   deleteService(url:any, data:any, token:boolean=false, httpOptions:any){   // here data:any is used in context of whatever data we are sending to backend through post operation
+  deleteService(url: any, data: any, token: boolean = false, httpOptions: any) {   // here data:any is used in context of whatever data we are sending to backend through post operation
     console.log(data)
-    return this.http.delete(this.baseurl+url,token && httpOptions)
-   }
+    return this.http.delete(this.baseurl + url, token && httpOptions)
+  }
 }
