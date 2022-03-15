4bad0a3a212c4ad98dad622ed4814bd9bf367aee
unknown <shaunak12wankhade@ggmail.com>
2022-03-15 16:14:48 +0530

    [Shaunak] Added: Refactored code for quickview component

--- src/app/component/quickview/quickview.component.ts
+++ src/app/component/quickview/quickview.component.ts
@@ -8,19 +8,19 @@ import { BookService } from 'src/app/services/bookservice/book.service';
   styleUrls: ['./quickview.component.scss']
 })
 export class QuickviewComponent implements OnInit {
-  bookdata:any;
+  bookdata: any;
 
-  bookid:any;  // used as a variable to store book id's used in activated route part below
+  bookid: any;  // used as a variable to store book id's used in activated route part below
 
-  feedbackArray:any;
-  feedback:any;
-  value:any;
+  feedbackArray: any;
+  feedback: any;
+  value: any;
 
   book_qty = 1;
-  addtobag:boolean=true;
-  quantity:boolean=false;
+  addtobag: boolean = true;
+  quantity: boolean = false;
 
-  constructor(private books:BookService, private activatedroute: ActivatedRoute, private route:Router) { }
+  constructor(private books: BookService, private activatedroute: ActivatedRoute, private route: Router) { }
 
   ngOnInit(): void {
 
@@ -32,61 +32,61 @@ export class QuickviewComponent implements OnInit {
     this.getbook();
     this.getfeedback();
   }
-  getbook(){
+  getbook() {
 
-    this.books.usergetallbooks().subscribe((response:any)=>{  // here even though the method is of getbook() we are calling the api integration of usergetallbooks() that is done previously in getallbooks.ts and then we are storing all books data in element by using forEach loop as below
-      response.result.forEach((element:any) => {  // in element we are storing our entire bookdetails 
+    this.books.usergetallbooks().subscribe((response: any) => {  // here even though the method is of getbook() we are calling the api integration of usergetallbooks() that is done previously in getallbooks.ts and then we are storing all books data in element by using forEach loop as below
+      response.result.forEach((element: any) => {  // in element we are storing our entire bookdetails 
 
-        if (element._id == this.bookid){  // here we are comparing entire book id (element._id) with one particular book id(this.bookid)
-          this.bookdata= element;  // we are storing our entire book details in element and assigning to this.bookdata that is coming from quickview.html (bookdata.bookName, bookdata.author)
+        if (element._id == this.bookid) {  // here we are comparing entire book id (element._id) with one particular book id(this.bookid)
+          this.bookdata = element;  // we are storing our entire book details in element and assigning to this.bookdata that is coming from quickview.html (bookdata.bookName, bookdata.author)
         }
-        
+
       });
 
     });
   }
 
-  addtocart(){
-    this.addtobag= false;
-    this.quantity=true;
-    this.books.useraddtobag(this.bookid).subscribe((response:any) =>{
+  addtocart() {
+    this.addtobag = false;
+    this.quantity = true;
+    this.books.useraddtobag(this.bookid).subscribe((response: any) => {
       console.log(response);
-      
+
     })
-     this.route.navigateByUrl('/dashboard/getcart')
+    this.route.navigateByUrl('/dashboard/getcart')
   }
-  addtowishlist(){
-    this.books.useraddtowishlist(this.bookid).subscribe((response:any) =>{
+  addtowishlist() {
+    this.books.useraddtowishlist(this.bookid).subscribe((response: any) => {
       console.log(response);
-      
+
     })
     this.route.navigateByUrl('/dashboard/getwishlist')
   }
 
-  addfeedback(){
-    let req={
-      comment:this.feedback,
-      rating:this.value
+  addfeedback() {
+    let req = {
+      comment: this.feedback,
+      rating: this.value
     }
-    this.books.useraddfeedback(this.bookid,req).subscribe((response:any)=>{
+    this.books.useraddfeedback(this.bookid, req).subscribe((response: any) => {
       console.log(response);
-      
+
     })
-  
+
   }
 
-  getfeedback(){
-    this.books.usergetfeedback(this.bookid).subscribe((response:any)=>{
+  getfeedback() {
+    this.books.usergetfeedback(this.bookid).subscribe((response: any) => {
       console.log(response.result);
-      this.feedbackArray=response.result;
+      this.feedbackArray = response.result;
       this.feedbackArray.reverse();
-      
+
     })
-    
+
   }
 
-  getShortName(fullName:any) { 
-    return fullName.split(' ').map((n:any) => n[0]).join('');
+  getShortName(fullName: any) {
+    return fullName.split(' ').map((n: any) => n[0]).join('');
   }
 
   negative() {
